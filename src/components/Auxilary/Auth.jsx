d415af395ea960064a820d8871f012deb66d15bd
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-02-19 14:34:39 -0700

    <Auth /> updating and refactoring of the component.

--- src/components/Auxilary/Auth.jsx
+++ src/components/Auxilary/Auth.jsx
@@ -1,249 +1,99 @@
-import React from "react";
+import React from 'react';
+
+
 import {
     auth,
-    db,
-    registerWithEmailAndPassword,
-    sendPasswordReset,
     signInWithGoogle,
-    logout
-} from "./firestore";
+    logout, getData,
+} from './firebase';
 
-export class Login extends React.Component {
+class Auth extends React.Component {
     constructor(props) {
         super(props);
-    }
 
-    state = {
-        email: "",
-        password: "",
-        user: null,
-        error: null,
-        loading: true
-    };
-
-    componentDidMount() {
-        auth.onAuthStateChanged(user => {
-            this.setState({user, loading: false});
-            if (user) {
-                this.props.history.push("/dashboard");
-            }
-        });
-    }
-
-    handleEmailChange = event => {
-        this.setState({email: event.target.value});
-    };
-
-    handlePasswordChange = event => {
-        this.setState({password: event.target.value});
-    };
-
-    handleLogin = () => {
-        registerWithEmailAndPassword(this.state.email, this.state.password);
-    };
-
-    handleGoogleLogin = () => {
-        signInWithGoogle();
-    };
-
-    render() {
-        return (
-            <div className="login">
-                <div className="login__container">
-                    <input
-                        type="text"
-                        className="login__textBox"
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
-                        placeholder="E-mail Address"
-                    />
-                    <input
-                        type="password"
-                        className="login__textBox"
-                        value={this.state.password}
-                        onChange={this.handlePasswordChange}
-                        placeholder="Password"
-                    />
-                    <button
-                        className="login__btn"
-                        onClick={this.handleLogin}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className="login__btn login__google"
-                        onClick={this.handleGoogleLogin}
-                    >
-                        Login with Google
-                    </button>
-                </div>
-            </div>
-        );
-    }
-}
-
-export class Register extends React.Component {
-    state = {
-        name: "",
-        email: "",
-        password: "",
-        user: null,
-        loading: true,
-        error: null
-    };
-
-    componentDidMount() {
-        auth.onAuthStateChanged((user) => {
-            this.setState({
-                user,
-                loading: false
-            });
-        });
-    }
-
-    handleNameChange = (e) => {
-        this.setState({name: e.target.value});
-    };
-
-    handleEmailChange = (e) => {
-        this.setState({email: e.target.value});
-    };
-
-    handlePasswordChange = (e) => {
-        this.setState({password: e.target.value});
-    };
-
-    handleRegister = () => {
-        if (!this.state.name) {
-            alert("Please enter name");
-            return;
-        }
-        registerWithEmailAndPassword(this.state.name, this.state.email, this.state.password);
-    };
-
-    render() {
-        return (
-            <div>
-                <input
-                    type="text"
-                    value={this.state.name}
-                    onChange={this.handleNameChange}
-                    placeholder="Full Name"
-                />
-                <input
-                    type="text"
-                    value={this.state.email}
-                    onChange={this.handleEmailChange}
-                    placeholder="E-mail Address"
-                />
-                <input
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.handlePasswordChange}
-                    placeholder="Password"
-                />
-                <button onClick={this.handleRegister}>Register</button>
-                <button onClick={signInWithGoogle}>Register with Google</button>
-            </div>
-        );
-    }
-}
-
-export class Reset extends React.Component {
-    state = {
-        email: "",
-    };
-
-    componentDidMount() {
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                this.props.history.push("/dashboard");
-            }
-        });
-    }
-
-    handleEmailChange = (event) => {
-        this.setState({email: event.target.value});
-    };
-
-    handlePasswordReset = () => {
-        sendPasswordReset(this.state.email);
-    };
-
-    render() {
-        return (
-            <div className="reset">
-                <div className="reset__container">
-                    <input
-                        type="text"
-                        className="reset__textBox"
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
-                        placeholder="E-mail Address"
-                    />
-                    <button
-                        className="reset__btn"
-                        onClick={this.handlePasswordReset}
-                    >
-                        Send password reset email
-                    </button>
-                </div>
-            </div>
-        );
-    }
-}
-
-export class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
         this.state = {
+            email: '',
+            password: '',
             user: null,
-            name: "",
             loading: true,
+            error: null,
         };
+
+        this.signIn = this.signIn.bind(this);
+    }
+
+    setToState = (...params) => {
+        let i = 0;
+
+        const recursiveSetToState = () => {
+            if (i >= params.length) return;
+
+            const key = params[i];
+
+            const value = params[i + 1];
+
+            i += 2;
+
+            this.setState({...this.state, [key]: value},
+                () => {
+                    recursiveSetToState()
+                }
+            );
+        }
+
+        recursiveSetToState();
+    }
+
+    async signIn() {
+        await signInWithGoogle();
+
+        const cvBase = await getData();
+
+        if (this.state.user) this.props.helper.setState('cvBase', cvBase);
     }
 
     componentDidMount() {
-        auth.onAuthStateChanged((user) => {
-            if (!user) {
-                this.props.history.push("/");
-            }
-            this.setState({user, loading: false});
-            this.fetchUserName();
-        });
+        auth.onAuthStateChanged(user => this.setToState('user', user));
     }
 
-    fetchUserName = () => {
-        db.collection("users")
-            .where("uid", "==", this.state.user.uid)
-            .get()
-            .then((querySnapshot) => {
-                const data = querySnapshot.docs[0].data();
-                this.setState({name: data.name});
-            })
-            .catch((error) => {
-                console.error(error);
-                alert("An error occured while fetching user data");
-            });
-    };
+    render = () => {
+        return (
+            <>
+                {this.state.user
+                    ? <Dashboard state={this.state}
+                                 history={this.props.history}
+                                 setToState={this.setToState}
+                    />
+                    : <>
+                        <button onClick={this.signIn}>Connect with Google</button>
+                    </>
+                }
+            </>
+        )
+    }
+}
 
+class Dashboard extends React.Component {
     logout = () => {
         logout();
+
+        this.props.setToState('user', null);
     };
 
     render() {
-        if (this.state.loading) {
-            return <div>Loading...</div>;
-        }
         return (
-            <div>
-                <div>
-                    Logged in as
-                    <div>{this.state.name}</div>
-                    <div>{this.state.user.email}</div>
-                    <button onClick={this.logout}>Logout</button>
-                </div>
-            </div>
+            <p>
+                {this.props.state.user !== null &&
+                    <>
+                        <img src={this.props.state.user.photoURL} alt='user profile picture'/>
+                        <span>Hello {this.props.state.user.displayName}</span>
+                        <span>{this.props.state.user.email}</span>
+                        <button onClick={this.logout}>Logout</button>
+                    </>
+                }
+            </p>
         );
     }
 }
+
+export default Auth;
