a2f87d17a4fc857ed7227d9d33662cb99134eacf
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-03-16 23:09:41 -0600

    minor update of firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,31 +1,21 @@
-// import firebase from 'firebase/app';
-import {initializeApp} from 'firebase/app';
-import "firebase/database";
+import { initializeApp } from 'firebase/app';
 import {
-    GoogleAuthProvider,
-    getAuth,
-    signInWithPopup,
-    signOut,
-} from "firebase/auth";
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
 
-import
-{
-    getDatabase,
-    ref,
-    child,
-    get,
-    set,
-    update,
-} from "firebase/database";
+import { getDatabase, ref, child, get, set, update } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -35,97 +25,93 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 const signInWithGoogle = async () => {
-    try {
-        const googleProvider = new GoogleAuthProvider();
+  try {
+    const googleProvider = new GoogleAuthProvider();
 
-        const result = await signInWithPopup(auth, googleProvider);
+    const result = await signInWithPopup(auth, googleProvider);
 
-        const user = result.user;
+    const user = result.user;
 
-        const snapshot = await get(child(ref(db), `users/${user.uid}`));
+    const snapshot = await get(child(ref(db), `users/${user.uid}`));
 
-        if (snapshot.exists()) {
-            await update(ref(db, `users/${user.uid}`), {
-                uid: user.uid,
-                name: user.displayName,
-                authProvider: "google",
-                email: user.email,
-            });
-        } else {
-            console.info("No data available");
-        }
-
-    } catch (error) {
-        alert(error.message);
-
-        console.error('Error authenticating with Google:', error);
+    if (snapshot.exists()) {
+      await update(ref(db, `users/${user.uid}`), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'google',
+        email: user.email,
+      });
+    } else {
+      console.info('No data available');
     }
+  } catch (error) {
+    alert(error.message);
+
+    console.error('Error authenticating with Google:', error);
+  }
 };
 
 const logout = () => {
-    signOut(auth);
+  signOut(auth);
 };
 
 const saveData = async (cvBase) => {
-    const user = await auth.currentUser;
+  const user = await auth.currentUser;
 
-    const localTimestamp = new Date().getTime();
+  const localTimestamp = new Date().getTime();
 
-    const setLs = () => localStorage.setItem('data', JSON.stringify({
+  const setLs = () =>
+    localStorage.setItem(
+      'data',
+      JSON.stringify({
         cvBase,
         localTimestamp,
-    }));
+      }),
+    );
 
-    if (!user) setLs();
-    else
-        try {
-            setLs();
+  if (!user) setLs();
+  else
+    try {
+      setLs();
 
-            await set(ref(db, `users/${user.uid}/data/`), {cvBase, localTimestamp});
-        } catch (error) {
-            alert(error.message)
+      await set(ref(db, `users/${user.uid}/data/`), { cvBase, localTimestamp });
+    } catch (error) {
+      alert(error.message);
 
-            console.error('Error saving data to database:', error);
-        }
-}
+      console.error('Error saving data to database:', error);
+    }
+};
 
 const getData = async () => {
-    const getLs = () => JSON.parse(localStorage.getItem('data'));
+  const getLs = () => JSON.parse(localStorage.getItem('data'));
 
-    const user = await auth.currentUser;
+  const user = await auth.currentUser;
 
-    if (!user) return getLs().cvBase;
-    else try {
-        const ls = getLs();
+  if (!user && getLs() !== null) return getLs().cvBase;
+  else if (getLs() === null || getLs() === undefined) return [];
+  else
+    try {
+      const ls = getLs();
 
-        const snapshot = await get(child(ref(db), `users/${user.uid}/data/`));
+      const snapshot = await get(child(ref(db), `users/${user.uid}/data/`));
 
-        if (ls.cvBase.length > 0 && snapshot.val()['cvBase'].length > 0) {
-            const lsTimestamp = ls.localTimestamp;
+      if (ls.cvBase.length > 0 && snapshot.val()['cvBase'].length > 0) {
+        const lsTimestamp = ls.localTimestamp;
 
-            const snapshotTimestamp = snapshot.val()['localTimestamp'];
+        const snapshotTimestamp = snapshot.val()['localTimestamp'];
 
-            return snapshotTimestamp > lsTimestamp
-                ? snapshot.val()['cvBase']
-                : ls.cvBase;
+        return snapshotTimestamp > lsTimestamp
+          ? snapshot.val()['cvBase']
+          : ls.cvBase;
+      } else if (snapshot.val()['cvBase'].length > 0)
+        return snapshot.val()['cvBase'];
+      else if (ls.cvBase.length > 0) return ls.cvBase;
+      else return [];
+    } catch (error) {
+      alert(error.message);
 
-        } else if (snapshot.val()['cvBase'].length > 0) return snapshot.val()['cvBase'];
-        else if (ls.cvBase.length > 0) return ls.cvBase;
-        else return [];
-
-    } catch
-        (error) {
-        alert(error.message)
-
-        console.error('Something went wrong:', error);
+      console.error('Something went wrong:', error);
     }
-}
-
-export {
-    auth,
-    db,
-    signInWithGoogle,
-    logout,
-    saveData,
-    getData,
 };
+
+export { auth, db, signInWithGoogle, logout, saveData, getData };
