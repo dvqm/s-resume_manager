4a427bca1f04367c310fc795d4fcbe7f2ff2b022
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-04-18 00:57:34 -0600

    Creating very basic structure for pdf

--- src/components/PdfLayout/PdfMarkup.jsx
+++ src/components/PdfLayout/PdfMarkup.jsx
@@ -7,6 +7,70 @@ class PdfResume extends React.Component {
 
     this.resume = props.resume;
   }
+  render() {
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    paddingHorizontal: 24,
+  },
+  section: {
+    marginBottom: 12,
+  },
+  heading: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  subheading: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    marginBottom: 8,
+  },
+});
+
+return <Document>
+    <Page size="A4" style={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.heading}>About</Text>
+        <View style={styles.divider}></View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.heading}>Summary</Text>
+        <View style={styles.divider}></View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.heading}>Area of Expertise</Text>
+        <View style={styles.divider}></View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.heading}>My Projects</Text>
+        <View style={styles.divider}></View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.heading}>Work Experience</Text>
+        <View style={styles.divider}></View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.heading}>Education</Text>
+        <View style={styles.divider}></View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.heading}>Additional information</Text>
+        <View style={styles.divider}></View>
+      </View>
+    </Page>
+  </Document>
+  }
 }
 
 export default PdfResume;
