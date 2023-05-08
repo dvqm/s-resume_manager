15b28aa99ea2c01f40c89488da49c2302b5ea23b
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-05-08 17:24:54 -0600

    Additional converted to FC

--- src/components/CurrentCv/Sections/Additional.jsx
+++ src/components/CurrentCv/Sections/Additional.jsx
@@ -2,52 +2,39 @@ import React from 'react';
 import { TextField, Typography } from '@mui/material';
 import { genericStyles } from './../../../mainTheme/localStyles';
 
-export class EditAdditional extends React.Component {
-  constructor(props) {
-    super(props);
+export const EditAdditional = ({titles, component, values, helper}) => {
+  return (
+    <>
+      <TextField
+        fullWidth
+        label={titles.title}
+        size='small'
+        value={values.title}
+        onChange={(e) => helper.onChange(component, 'name', e, values.id)}
+      />
 
-    this.key = props.keyName;
-
-    this.id = props.section.id;
-
-    this.handleChange = props.helper.onChange;
-  }
-
-  render() {
-    return (
-      <>
-        <TextField
-          fullWidth
-          label={this.props.static.title}
-          size='small'
-          value={this.props.section.title}
-          onChange={(e) => this.handleChange(this.key, 'name', e, this.id)}
-        />
-
-        <TextField
-          multiline
-          minRows={8}
-          size='small'
-          label={this.props.static.description}
-          value={this.props.section.description}
-          onChange={(e) =>
-            this.handleChange(this.key, 'description', e, this.id)
-          }
-        />
-      </>
-    );
-  }
+      <TextField
+        multiline
+        minRows={8}
+        size='small'
+        label={titles.description}
+        value={values.description}
+        onChange={(e) =>
+          helper.onChange(component, 'description', e, values.id)
+        }
+      />
+    </>
+  );
 }
 
-export class ViewAdditional extends React.Component {
-  render() {
-    const { PreBlock } = genericStyles;
-    return (
-      <>
-        <PreBlock variant='body1' component='pre'>
-          {this.props.section.description}
-        </PreBlock>
-      </>
-    );
-  }
+export const ViewAdditional = ({values}) => {
+  const { PreBlock } = genericStyles;
+
+  return (
+    <>
+      <PreBlock variant='body1' component='pre'>
+        {values.description}
+      </PreBlock>
+    </>
+  );
 }
