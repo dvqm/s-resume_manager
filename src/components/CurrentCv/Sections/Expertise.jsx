72074f340ba8e1f415cfec682942371006f27ae2
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-05-08 17:23:32 -0600

    Expertise converted to FC

--- src/components/CurrentCv/Sections/Expertise.jsx
+++ src/components/CurrentCv/Sections/Expertise.jsx
@@ -1,64 +1,51 @@
 import React from 'react';
-import { Chip, TextField, Stack } from '@mui/material';
+import { Chip, Stack } from '@mui/material';
 import LabelImportantOutlinedIcon from '@mui/icons-material/LabelImportantOutlined';
 import { expertiseStyles } from './../../../mainTheme/localStyles';
 
 const { TextStyled } = expertiseStyles;
 
-export class EditExpertise extends React.Component {
-  constructor(props) {
-    super(props);
+export const EditExpertise = ({ component, titles, values, helper }) => {
 
-    this.key = props.keyName;
+  return (
+    <>
+      <TextStyled
+        type='text'
+        placeholder={titles.placeholder}
+        value={values.title}
+        onChange={(e) => helper.onChange(component, 'title', e, values.id)}
+      />
 
-    this.id = props.section.id;
-
-    this.handleChange = props.helper.onChange;
-  }
-
-  render() {
-    return (
-      <>
-        <TextStyled
-          type='text'
-          placeholder={this.props.static.placeholder}
-          value={this.props.section.title}
-          onChange={(e) => this.handleChange(this.key, 'title', e, this.id)}
-        />
-
-        <TextStyled
-          multiline
-          rows={8}
-          placeholder={this.props.static.tip}
-          value={this.props.section.labels}
-          onChange={(e) => this.handleChange(this.key, 'labels', e, this.id)}
-        />
-      </>
-    );
-  }
+      <TextStyled
+        multiline
+        rows={8}
+        placeholder={titles.tip}
+        value={values.labels}
+        onChange={(e) => helper.onChange(component, 'labels', e, values.id)}
+      />
+    </>
+  );
 }
 
-export class ViewExpertise extends React.Component {
-  labelsParse(labelsStr) {
+export const ViewExpertise = ({ values }) => {
+  const labelsParse = (labelsStr) => {
     return labelsStr.split(', ');
-  }
+  };
 
-  render() {
-    const labels = this.labelsParse(this.props.section.labels);
+  const labels = labelsParse(values.labels);
 
-    return (
-      <Stack direction='row' sx={{ flexWrap: 'wrap' }}>
-        {labels.map((label, index) => (
-          <Chip
-            variant='skill'
-            color='secondary'
-            key={index}
-            icon={<LabelImportantOutlinedIcon sx={{ fontSize: '1.5em' }} />}
-            label={label}
-            sx={{ mx: 1, my: 1 }}
-          />
-        ))}
-      </Stack>
-    );
-  }
+  return (
+    <Stack direction='row' sx={{ flexWrap: 'wrap' }}>
+      {labels.map((label, index) => (
+        <Chip
+          variant='skill'
+          color='secondary'
+          key={index}
+          icon={<LabelImportantOutlinedIcon sx={{ fontSize: '1.5em' }} />}
+          label={label}
+          sx={{ mx: 1, my: 1 }}
+        />
+      ))}
+    </Stack>
+  );
 }
