b219fc7179386e0ccbd090e54876c130cf4704a2
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-09-24 23:28:05 -0600

    refactoring of ResumeList component

--- src/components/ManageContents/ContentsResumeList.jsx
+++ src/components/ManageContents/ContentsResumeList.jsx
@@ -1,98 +1,43 @@
 import React from 'react';
-import mainTheme from '../../mainTheme/globalTheme.js';
 import { contentsStyles } from '../../mainTheme/localStyles';
-import { Button, ListItem, ListItemText, IconButton } from '@mui/material';
+import { ListItemText } from '@mui/material';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import MockBtns from './ResumeListMockBtns.jsx';
 
-const ContentsResumesList = ({currentResume, resumes, deleteMock, addMock}) => {
-  const { BoxStyled,ListStyled, ListBtn, ListBtnSelected } = contentsStyles;
+const ContentsResumesList = ({ accessResume, accessResumes, deleteMock, addMock, accessList }) => {
+  const { ListStyled, ListBtn, ListBtnSelected } = contentsStyles;
 
-    const mdBreakpoint = mainTheme.breakpoints.values.md;
-    const mockBtns = () => {
-      const styles = {
-        btn: {
-          border: 'none',
-          fontSize: 10,
-          '&:hover': {
-            border: 'none',
-          },
-        },
-        listItem: {
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'nowrap',
-          justifyContent: 'space-between',
-        },
-      };
-
-      return (
-      <BoxStyled>
-        <ListItem sx={styles.listItem}>
-          {resumes.some((cv) => cv.mock) ? (
-            <Button
-              sx={styles.btn}
-              variant='outlined'
-              color='primary'
-              size='small'
-              onClick={deleteMock}
-            >
-              Delete Demo
-            </Button>
-          ) : (
-              <Button
-                sx={styles.btn}
-                variant='outlined'
-                color='primary'
-                size='small'
-                onClick={addMock}
-              >
-                Load Demo
-              </Button>
-            )}
-          {window.innerWidth <= mdBreakpoint &&
-            <IconButton /*onClick={}*/>
-              <CloseOutlinedIcon />
-            </IconButton>
-          }
-        </ListItem>
-      </BoxStyled>
-      );
-    };
-
-    return (
+  return <ListStyled>
+    {accessResumes().length > 0 ? (
       <>
-        <ListStyled>
-          {resumes.length > 0 ? (
-            <>
-              {mockBtns()}
-              {resumes.map((resume) => (
-                <React.Fragment key={resume.cvName}>
-                  {currentResume.cvName === resume.cvName ? (
-                    <ListBtnSelected /*onClick={handleChooseCv}*/>
-                      <ArticleOutlinedIcon />
-                      <ListItemText>{resume.cvName}</ListItemText>
-                    </ListBtnSelected>
-                  ) : (
-                    <ListBtn /*onClick={handleChooseCv}*/>
-                      <ArticleOutlinedIcon />
-                      <ListItemText>{resume.cvName}</ListItemText>
-                    </ListBtn>
-                  )}
-                </React.Fragment>
-              ))}
-            </>
-          ) : (
-            <>
-              {mockBtns()}
-              <ListItemText sx={{ textAlign: 'center' }}>
-                No saved resumes
-              </ListItemText>
-            </>
-          )}
-        </ListStyled>
+        <MockBtns deleteMock={deleteMock} addMock={addMock}
+          accessResumes={accessResumes} accessList={accessList} />
+        {accessResumes().map((resume) => (
+          <React.Fragment key={resume.name}>
+            {accessResume().name === resume.name ? (
+              <ListBtnSelected /*onClick={handleChooseCv}*/>
+                <ArticleOutlinedIcon />
+                <ListItemText>{resume.name}</ListItemText>
+              </ListBtnSelected>
+            ) : (
+              <ListBtn /*onClick={handleChooseCv}*/>
+                <ArticleOutlinedIcon />
+                <ListItemText>{resume.name}</ListItemText>
+              </ListBtn>
+            )}
+          </React.Fragment>
+        ))}
       </>
-    );
-  };
+    ) : (
+      <>
+        <MockBtns deleteMock={deleteMock} addMock={addMock}
+          accessResumes={accessResumes} accessList={accessList} />
+        <ListItemText sx={{ textAlign: 'center' }}>
+          No saved resumes
+        </ListItemText>
+      </>
+    )}
+  </ListStyled>
+};
 
 export default ContentsResumesList;
