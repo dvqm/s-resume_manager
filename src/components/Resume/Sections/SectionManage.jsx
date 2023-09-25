79a513460ea1044f30094b6a7343bf0efa05b3f9
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-09-24 23:27:22 -0600

    Refactoring of SectionManage component

--- src/components/Resume/Sections/SectionManage.jsx
+++ src/components/Resume/Sections/SectionManage.jsx
@@ -1,15 +1,18 @@
-import { FormGroup, Divider, IconButton, Typography } from "@mui/material";
-import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
-import EditIcon from '@mui/icons-material/Edit';
-import { genericStyles } from "../../../mainTheme/localStyles";
-import { createElement, useState } from 'react';
+import { useEffect, useState } from 'react';
+import SectionEdit from "./SectionEdit";
+import SectionView from "./SectionView";
+import { enterEdit, exitEdit, observeEdit } from '../../../functions/EditableObserver';
 
 const SectionManage = ({ field, setFields, updateResume, viewer, editer, titles, id, initial }) => {
   const [original, setOriginal] = useState([]);
   const [init, setInit] = useState(true);
   const [isEdit, setIsEdit] = useState(initial);
+
+ useEffect(() => {
+    observeEdit(isEdit);
+
+    return () => observeEdit(isEdit);
+  }, [isEdit]);
 
   const actions = new Map([
     ['delete', () => {
@@ -53,83 +56,18 @@ const SectionManage = ({ field, setFields, updateResume, viewer, editer, titles,
   return <>
     {
       isEdit ? (
-        <SectionEdit
-          init={init}
-          editer={editer}
-          titles={titles}
-          view={field}
-          field={field}
-          setFields={setFields}
-          handleAction={handleAction}
+        <SectionEdit init={init}
+          editer={editer} titles={titles}
+          view={field} field={field}
+          setFields={setFields} handleAction={handleAction}
         />
       ) : (
-        <SectionView
-          viewer={viewer}
-          field={field}
-          titles={titles}
+        <SectionView viewer={viewer}
+          field={field} titles={titles}
           handleAction={handleAction}
         />
       )}
   </>
 };
-
-const SectionEdit = ({ editer, titles, field, handleAction, init }) => {
-  const { ManageBtnsWrapper } = genericStyles;
-
-  return <form
-    onSubmit={(e) => handleAction(e, 'save')}
-  >
-    <FormGroup>
-      <ManageBtnsWrapper>
-        <IconButton color='secondary' type='submit'>
-          <CheckOutlinedIcon />
-        </IconButton>
-        {!init && (
-          <IconButton
-            color='secondary'
-            onClick={(e) => handleAction(e, 'cancel')}
-          >
-            <CloseOutlinedIcon />
-          </IconButton>
-        )}
-        <IconButton
-          color='secondary'
-          onClick={(e) => handleAction(e, 'delete')}
-        >
-          <DeleteForeverOutlinedIcon />
-        </IconButton>
-      </ManageBtnsWrapper>
-
-      {createElement(editer, {
-        titles: titles,
-        field: field,
-        handleAction: handleAction,
-      })}
-    </FormGroup>
-  </form>
-}
-
-const SectionView = ({ viewer, field, titles, handleAction }) => {
-  const { ArticleStyled, StackRow } = genericStyles;
-  return <ArticleStyled>
-    <StackRow>
-      <Typography variant='h3'>{field.title}</Typography>
-
-      <IconButton
-        color='secondary'
-        size='medium'
-        onClick={(e) => handleAction(e, 'edit')}
-      >
-        <EditIcon />
-      </IconButton>
-    </StackRow>
-
-    {createElement(viewer, {
-      titles: titles,
-      field: field,
-    })}
-    <Divider light variant='inset' />
-  </ArticleStyled>
-}
 
 export default SectionManage;
