7e3f768846065702df9c2a3c039be530ff32b5a8
dvqm <10248779+dvqm@users.noreply.github.com>
2024-03-14 00:31:09 -0600

    removing .env and moving firebase credentials to the firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -9,14 +9,13 @@ import {
 import { getDatabase, ref, child, get, set, update } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+ apiKey: 'AIzaSyDp-DGGNnymJqCVdL5NnmDWuFbxQ9gt-Kc',
+  authDomain: 'will-test-sandbox.firebaseapp.com',
+  databaseURL: 'https://will-test-sandbox-default-rtdb.firebaseio.com',
+  projectId: 'will-test-sandbox',
+  storageBucket: 'will-test-sandbox.appspot.com',
+  messagingSenderId: '317616172884',
+  appId: '1:317616172884:web:7fe4c67190424bd47a22c1'
 };
 
 const app = initializeApp(firebaseConfig);
