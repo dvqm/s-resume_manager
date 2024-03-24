0211bc9391b72b6efc8954cd02f5d4c61f49cecd
dvqm <10248779+dvqm@users.noreply.github.com>
2024-03-23 22:50:34 -0600

    removing deleted web-vitals from indes.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import ContextProvider from './state/context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ContextProvider from "./state/context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-reportWebVitals();
