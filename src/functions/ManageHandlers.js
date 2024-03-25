fdac55838a564c34e83e6fb4914bd41e0fe55538
dvqm <10248779+dvqm@users.noreply.github.com>
2024-03-24 22:03:54 -0600

    formatting code

--- src/functions/ManageHandlers.js
+++ src/functions/ManageHandlers.js
@@ -1,26 +1,32 @@
-const ManageHandlers = (resume, resumes, resumeDispatch, resumesDispatch, accessOriginal) => ({
+const ManageHandlers = (
+  resume,
+  resumes,
+  resumeDispatch,
+  resumesDispatch,
+  accessOriginal,
+) => ({
   restore() {
-    resumeDispatch({ t: 'RES_UPD', p: accessOriginal() });
+    resumeDispatch({ t: "RES_UPD", p: accessOriginal() });
   },
 
   new() {
-    resumeDispatch({ t: 'RES_NEW' });
+    resumeDispatch({ t: "RES_NEW" });
   },
 
   delete() {
-    resumesDispatch({ t: 'RES_DEL', p: resume.name });
-    resumeDispatch({ t: 'RES_NEW' });
+    resumesDispatch({ t: "RES_DEL", p: resume.name });
+    resumeDispatch({ t: "RES_NEW" });
   },
 
   clone() {
-    if (resume.name === '') return alert('Resume haven\'t been saved');
-    if (accessOriginal().name === '') return alert('Resume haven\'t been saved');
+    if (resume.name === "") return alert("Resume haven't been saved");
+    if (accessOriginal().name === "") return alert("Resume haven't been saved");
 
     const makeDuplicate = (name) => {
       if (resumes.every((item) => item.name !== name)) {
         const clonedResume = { ...resume, name };
-        resumesDispatch({ t: 'RES_SAVE', p: clonedResume });
-        resumeDispatch({ t: 'RES_UPD', p: clonedResume });
+        resumesDispatch({ t: "RES_SAVE", p: clonedResume });
+        resumeDispatch({ t: "RES_UPD", p: clonedResume });
         return;
       } else {
         makeDuplicate(`${name} _copy`);
@@ -28,6 +34,6 @@ const ManageHandlers = (resume, resumes, resumeDispatch, resumesDispatch, access
     };
     makeDuplicate(resume.name);
   },
-})
+});
 
 export default ManageHandlers;
