0616e3abc26c97a60bc9e5be48c1720aec6ac7bf
dvqm <10248779+dvqm@users.noreply.github.com>
2024-03-24 22:01:41 -0600

    adding to the dispatcher functions localStorage interaction and syncData call

--- src/state/resumeReducer.jsx
+++ src/state/resumeReducer.jsx
@@ -1,23 +1,26 @@
-import { defaultValues, template } from './templates';
+import { syncData } from "../database/firebase";
+import { defaultValues, template } from "./templates";
 
 const resumeReducer = (s, a) => {
   const handlers = [
     {
-      t: 'RES_UPD',
-      handler: (_, p) => ({ ...p })
+      t: "RES_UPD",
+      handler: (_, p) => {
+        syncData()
+        return { ...p } },
     },
 
     {
-      t: 'RES_NAME',
-      handler: (s, p) => ({ ...s, name: p, })
+      t: "RES_NAME",
+      handler: (s, p) => ({ ...s, name: p }),
     },
     {
-      t: 'RES_NEW',
-      handler: () => ({ ...template })
+      t: "RES_NEW",
+      handler: () => ({ ...template }),
     },
 
     {
-      t: 'ART_CHANGE',
+      t: "ART_CHANGE",
       handler: (s, p) => {
         const [rubric, value] = p;
         return {
@@ -25,50 +28,49 @@ const resumeReducer = (s, a) => {
           [rubric]: {
             ...s[rubric],
             ...value,
-          }
-        }
-      }
+          },
+        };
+      },
     },
 
     {
-      t: 'ART_UPD',
+      t: "ART_UPD",
       handler: (s, p) => {
         const [rubric, value] = p;
         return {
           ...s,
           [rubric]: value,
-        }
-      }
+        };
+      },
     },
 
     {
-      t: 'ART_CLEAR',
+      t: "ART_CLEAR",
       handler: (s, p) => {
         const [rubric] = p;
-        const cleared = Object
-          .fromEntries(Object.keys(s[rubric])
-            .map(key => [key, '']));
+        const cleared = Object.fromEntries(
+          Object.keys(s[rubric]).map((key) => [key, ""]),
+        );
         return {
           ...s,
-          [rubric]: cleared
-        }
-      }
+          [rubric]: cleared,
+        };
+      },
     },
 
     {
-      t: 'SEC_ADD',
+      t: "SEC_ADD",
       handler: (s, p) => {
         const [rubric] = p;
         return {
           ...s,
           [rubric]: [...s[rubric], defaultValues[rubric]],
-
-        }
-      }
+        };
+      },
     },
 
     {
-      t: 'SEC_UPD',
+      t: "SEC_UPD",
       handler: (s, p) => {
         const [rubric, index, field] = p;
         return {
@@ -78,44 +80,44 @@ const resumeReducer = (s, a) => {
               return {
                 ...item,
                 ...field,
-              }
+              };
             }
             return item;
-          })
-        }
-      }
+          }),
+        };
+      },
     },
 
     {
-      t: 'SEC_RESTORE',
+      t: "SEC_RESTORE",
       handler: (s, p) => {
         const [rubric, index, value] = p;
         return {
           ...s,
-          [rubric]: s[rubric].map((item, i) => i === index ? value : item)
+          [rubric]: s[rubric].map((item, i) => (i === index ? value : item)),
         };
-      }
+      },
     },
 
     {
-      t: 'SEC_DEL',
+      t: "SEC_DEL",
       handler: (s, p) => {
         const [rubric, index] = p;
         return {
           ...s,
-          [rubric]: s[rubric].filter((_, i) => i !== index)
-        }
-      }
-    }
+          [rubric]: s[rubric].filter((_, i) => i !== index),
+        };
+      },
+    },
   ];
 
-  const actions = handlers.find(handler => handler.t === a.t);
+  const actions = handlers.find((handler) => handler.t === a.t);
 
   if (a) {
     return actions.handler(s, a.p);
   }
 
   return s;
-}
+};
 
 export default resumeReducer;
--- src/state/resumesReducer.jsx
+++ src/state/resumesReducer.jsx
@@ -1,58 +1,89 @@
-import { examples } from '../state/templates';
+import { examples } from "../state/templates";
+
+const setLocalStorage = (data) => {
+  localStorage.setItem(
+    "data",
+    JSON.stringify({
+      resumes: data,
+      timestamp: new Date().getTime(),
+    }),
+  );
+};
 
 const resumesReducer = (s, a) => {
   const handlers = [
     {
-      t: 'RES_LOAD',
+      t: "RES_LOAD",
       handler: (_, p) => [...p],
     },
 
     {
-      t: 'RES_SAVE',
+      t: "RES_SAVE",
       handler: (s, p) => {
-        const index = s.findIndex((item) => item.name === p.name)
-        return index === -1 ? [...s, p] : s;
-      }
+        const index = s.findIndex((item) => item.name === p.name);
+        const result = index === -1 ? [...s, p] : s;
+        setLocalStorage(result);
+        return result;
+      },
     },
 
     {
-      t: 'RES_UPDATE',
-      handler: (s, p) => [...s.map((item) => item.name === p.originalName ? p.resume : item)]
+      t: "RES_UPDATE",
+      handler: (s, p) => {
+        const result = [
+          ...s.map((item) => (item.name === p.originalName ? p.resume : item)),
+        ];
+        setLocalStorage(result);
+        return result;
+      },
     },
 
     {
-      t: 'RES_DEL',
-      handler: (s, p) => s.filter((item) => item.name !== p)
+      t: "RES_DEL",
+      handler: (s, p) => {
+        const result = s.filter((item) => item.name !== p);
+        setLocalStorage(result);
+        return result;
+      },
     },
 
     {
-      t: 'RES_ADD_MOCK',
+      t: "RES_ADD_MOCK",
       handler: (s) => {
-        const resumesMap = new Map(s.map(resume => resume.hasOwnProperty('mock')
-          ? [resume.mock, resume]
-          : [resume.name, resume]));
-        examples.forEach(mockedResume => {
+        const resumesMap = new Map(
+          s.map((resume) =>
+            resume.hasOwnProperty("mock")
+              ? [resume.mock, resume]
+              : [resume.name, resume],
+          ),
+        );
+        examples.forEach((mockedResume) => {
           resumesMap.set(mockedResume.mock, mockedResume);
         });
+
+        setLocalStorage(Array.from(resumesMap.values()));
+
         return Array.from(resumesMap.values());
-      }
+      },
     },
 
     {
-      t: 'RES_DEL_MOCK',
+      t: "RES_DEL_MOCK",
       handler: (s) => {
-        return s.filter((resume) => !resume.hasOwnProperty('mock'));
-      }
+        const result = s.filter((resume) => !resume.hasOwnProperty("mock"));
+        setLocalStorage(result);
+        return result;
+      },
     },
   ];
 
-  const actions = handlers.find(handler => handler.t === a.t);
+  const actions = handlers.find((handler) => handler.t === a.t);
 
   if (a) {
     return actions.handler(s, a.p);
   }
 
   return s;
-}
+};
 
 export default resumesReducer;
